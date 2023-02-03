e02f4c18799e7a5666e5a2ea96c342a4c43ea014
Reece Taylor <reece.taylor@live.com>
2023-02-03 13:40:43 +0000

    init upload

---
+++ todo.go
@@ -0,0 +1,134 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/alexeyco/simpletable"
+)
+
+// create struct for items
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item
+
+// func to add tasks
+func (t *Todos) Add(task string) {
+	todo := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	// append the data
+	*t = append(*t, todo)
+}
+
+// when using -complete flag with index of task
+func (t *Todos) Complete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("invalid index")
+	}
+
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
+
+}
+
+// -del at index to delete a task
+func (t *Todos) Delete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("invalid index")
+	}
+
+	*t = append(ls[:index-1], ls[index:]...)
+
+	return nil
+}
+
+// read the json file
+func (t *Todos) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// stores the info to json file
+func (t *Todos) Store(filename string) error {
+
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+// prints out the table nice and pretty, tried to use simpletable lib saw on yt vid
+func (t *Todos) Print() {
+
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done"},
+			{Align: simpletable.AlignRight, Text: "Created At"},
+			{Align: simpletable.AlignRight, Text: "Completed At"},
+		},
+	}
+
+	var cells [][]*simpletable.Cell
+
+	for idx, item := range *t {
+		idx++
+		cells = append(cells, *&[]*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", idx)},
+			{Text: item.Task},
+			{Text: fmt.Sprintf("%t", item.Done)},
+			{Text: item.CreatedAt.String()},
+			{Text: item.CreatedAt.Format(time.RFC1123)},
+		})
+	}
+
+	table.Body = &simpletable.Body{Cells: cells}
+
+	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+		{Align: simpletable.AlignCenter, Span: 5, Text: "Todos:"},
+	}}
+
+	table.SetStyle(simpletable.StyleRounded)
+
+	table.Println()
+
+}
